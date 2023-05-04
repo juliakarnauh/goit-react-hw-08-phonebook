9c13fe247d3d655dfeb29b6fad915908a9cbdeed
juliakarnaukh <juliakarnayh04@gmail.com>
2023-05-04 19:00:29 +0300

    fix

--- src/redux/Auth/authSelections.jsx
+++ src/redux/Auth/authSelections.jsx
@@ -1,3 +1,2 @@
 export const selectUserEmail = state => state.auth.user.email;
 export const selectOnline = state => state.auth.online;
-export const selectUserLoading = state => state.auth.loading;
